edfa274e4ecd3649f1e27186f6105074fbc9e155
Juan Quezada <ju.quezada.c@gmail.com>
2021-09-08 00:45:08 -0300

    commit

--- assets/js/Scripts.js
+++ assets/js/Scripts.js
@@ -1,26 +1,43 @@
-window.onload = function() {
+$(document).ready(function(){
+    $.ajax({
+        type: "GET",
+        url: "https://www.superheroapi.com/.​",
+        dataType: "json",
+        success: function (datos){
+            console.log(datos);
+        },
+        error: function (error) {
+            console.log(error);
+        },
+    });
+});
+
+
+
+
+window.onload = function () {
 
     var options = {
         title: {
             text: "Estadisticas de Poder Para "
         },
         data: [{
-                Nombre: "pie",
-                startAngle: 45,
-                showInLegend: "true",
-                legendText: "{label}",
-                indexLabel: "{label} ({y})",
-                yValueFormatString:"#,##0.#"%"",
-                dataPoints: [
-                    { label: "Inteligencia", y: 36 },
-                    { label: "Fuerza", y: 31 },
-                    { label: "Velocidad", y: 7 },
-                    { label: "Duravilidad", y: 7 },
-                    { label: "Poder", y: 6 },
-                    { label: "Combate", y: 10 },
-                ]
+            Nombre: "pie",
+            startAngle: 45,
+            showInLegend: "true",
+            legendText: "{label}",
+            indexLabel: "{label} ({y})",
+            yValueFormatString: "#,##0.#" % "",
+            dataPoints: [
+                { label: "Inteligencia", y: 36 },
+                { label: "Fuerza", y: 31 },
+                { label: "Velocidad", y: 7 },
+                { label: "Duravilidad", y: 7 },
+                { label: "Poder", y: 6 },
+                { label: "Combate", y: 10 },
+            ]
         }]
     };
     $("#chartContainer").CanvasJSChart(options);
-    
-    }
+
+}
